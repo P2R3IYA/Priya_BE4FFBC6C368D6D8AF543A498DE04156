c8b88cf5a4de133c0601f414949a3d8364c6fe80
P2R3IYA <144204183+P2R3IYA@users.noreply.github.com>
2023-09-07 11:40:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def CheckLeap(Year):
     print("Given Year is a leap Year");
   else:  
     print ("Given Year is not a leap Year")  
-Year = int(input("Enter the number: "))  
+Year = int(input("Enter a year: "))  
 CheckLeap(Year)  
